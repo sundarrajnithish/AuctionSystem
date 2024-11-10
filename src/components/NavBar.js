7a25ce0f6afb1a678a6d02981cbb957ed7989d83
Nitish Sundarraj <sundarrajnithish@gmail.com>
2024-11-10 17:45:16 -0500

    Removed Signing for Dev

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Authenticator } from '@aws-amplify/ui-react';
+//import { Authenticator } from '@aws-amplify/ui-react';
 import './NavBar.css';
 
 const NavBar = () => {
   return (
-    <Authenticator>
-      {({ signOut }) => ( // Destructure the signOut function from Authenticator
+    //<Authenticator>
+      //{({ signOut }) => ( // Destructure the signOut function from Authenticator
         <nav className="navbar" style={{ backgroundColor: 'blue', color: 'white' }}>
           <div className="navbar-logo">
             <Link to="/">Modern Auction</Link>
@@ -17,16 +17,19 @@ const NavBar = () => {
             <li><Link to="/analytics">Cars</Link></li>
             <li><Link to="/dashboard">Luxuries</Link></li>
             <li><Link to="/about">Historical</Link></li>
-            <li>
-              <button onClick={signOut} style={{ backgroundColor: 'transparent', color: 'white', border: 'none', cursor: 'pointer' }}>
-                Sign Out
-              </button>
-            </li>
+            
           </ul>
         </nav>
-      )}
-    </Authenticator>
+      //)}
+    //</Authenticator>
   );
 };
+
+// Removed Singout Button
+//<li>
+// <button onClick={signOut} style={{ backgroundColor: 'transparent', color: 'white', border: 'none', cursor: 'pointer' }}>
+// Sign Out
+// </button>
+// </li>
 
 export default NavBar;
