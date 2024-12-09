d038d47982b0d3837b71b7f65dba4fa34f7b86b2
Nitish Sundarraj <sundarrajnithish@gmail.com>
2024-12-09 17:58:37 -0500

    Deploy Issue Fix 1

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './styles/Navbar.css';
+import './Navbar.css';
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
