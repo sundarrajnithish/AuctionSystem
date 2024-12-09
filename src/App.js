ce9248a019e6e5c32f650f63482266b447290485
Nitish Sundarraj <sundarrajnithish@gmail.com>
2024-12-09 18:04:57 -0500

    Fix 3

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 // Components
-import Navbar from './components/navbar';
+import Navbar from './components/navbar.js';
 
 // Pages
 import Auctions from './pages/Auctions';
