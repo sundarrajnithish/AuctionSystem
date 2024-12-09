677b824527add834e39fef89aa891f1ea309fff1
Nitish Sundarraj <sundarrajnithish@gmail.com>
2024-12-09 18:02:49 -0500

    Fix 2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 // Components
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar';
 
 // Pages
 import Auctions from './pages/Auctions';
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Navbar.css';
+import './navbar.css';
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
